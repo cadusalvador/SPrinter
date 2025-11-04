ba455e37a4a2ad644001f25b1c97d60428617790
cadusalvador <carlos1992.ces@gmail.com>
2025-11-04 05:48:04 -0300

    refatoração driver.py

--- utils/driver.py
+++ utils/driver.py
@@ -1,9 +1,13 @@
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from utils.helpers import log
 
 def create_chrome_driver(config, headless=True):
+
+    chromedriver_autoinstaller.install()
+
     chrome_options = Options()
     if headless:
         chrome_options.add_argument("--headless=new")
@@ -16,11 +20,9 @@ def create_chrome_driver(config, headless=True):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     
-    chrome_driver_path = config["CHROME_DRIVER_PATH"]
     page_load_timeout = int(config.get("PAGE_LOAD_TIMEOUT", 30))
 
-    service = Service(chrome_driver_path)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.set_page_load_timeout(page_load_timeout)
     log("✅ Driver iniciado e navegando para: ", config["LOOKER_URL_VISAO_GERAL"])
