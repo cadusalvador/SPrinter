560f3b22ee716a7fe9ee85b876c90dadebfc16c5
cadusalvador <carlos1992.ces@gmail.com>
2025-11-05 03:53:03 -0300

    Atualização do drive.py: atualiza o ChromeDriver automaticamente

--- utils/driver.py
+++ utils/driver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from utils.helpers import log
 
 def create_chrome_driver(config, headless=True):
@@ -15,13 +16,15 @@ def create_chrome_driver(config, headless=True):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-
-    chrome_driver_path = config["CHROME_DRIVER_PATH"]
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-infobars")
+    
     page_load_timeout = int(config.get("PAGE_LOAD_TIMEOUT", 30))
 
-    service = Service(chrome_driver_path)
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.set_page_load_timeout(page_load_timeout)
     log(f'✅ Driver iniciado e navegando para: {config["LOOKER_URL_VISAO_GERAL"]}')
     return driver
+
